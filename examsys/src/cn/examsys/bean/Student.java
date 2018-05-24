0e91852292b034b78829f0892c5bc5a81b4fa2c7
lrx <601740275@qq.com>
2018-05-24 17:09:58 +0800

    2

--- examsys/src/cn/examsys/bean/Student.java
+++ examsys/src/cn/examsys/bean/Student.java
@@ -8,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Generated;
-
 @Entity
 @Table(name="student")
 public class Student {
