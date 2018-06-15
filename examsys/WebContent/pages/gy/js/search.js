3fabc4045c2ed8d0274d2b158afcca7aa351cb88
huajianyi <1770313147@qq.com>
2018-06-15 10:02:56 +0800

    ttt

---
+++ examsys/WebContent/pages/gy/js/search.js
@@ -0,0 +1,62 @@
+//条件搜索
+$(document).ready(function(){
+	$("#find").click(function(){
+		$("#hide").slideToggle("slow");
+			$("#school").click(function(){
+				$("#school1").slideToggle("slow");
+			});
+			$("#institute").click(function(){
+				$("#institute1").slideToggle("slow");
+			});
+			$("#profession").click(function(){
+				$("#profession1").slideToggle("slow");
+			});
+			$("#class").click(function(){
+				$("#class1").slideToggle("slow");
+			});
+	});
+});
+
+//导出历史成绩
+//$(function(){  
+//  $("#selectAll").click(function(){  
+// 		$("#checkBoxList :checkbox").attr("checked", true);  
+//  });  
+//  $("#unSelect").click(function(){  
+//      $("#checkBoxList :checkbox").attr("checked", false);  
+//  });  
+//  $("#reverseSelect").click(function(){  
+//      $("#checkBoxList :checkbox").each(function(){  
+//          $(this).attr("checked", !$(this).attr("checked"));  
+//          });  
+//      });  
+//  });
+    
+//导出历史成绩
+
+	//获取全选框复选框
+	//var checkbox_all_select=document.getElementById("checkbox_all_select");
+	$("#checkbox_all_select").click(function(){
+		var checkbox_select=document.getElementsByName("checkbox_select");
+		for(var i=0;i<checkbox_select.length;i++){
+			checkbox_select[i].checked=true;
+		}
+	});
+	//获取全不选框复选框
+	$("#checkbox_select").click(function(){
+		var checkbox_select=document.getElementsByName("checkbox_select");
+		for(var i=0;i<checkbox_select.length;i++){
+			checkbox_select[i].checked=false;
+		}
+	});
+	//获取反选框复选框
+	$("#reverseSelect").click(function(){
+		var checkbox_select=document.getElementsByName("checkbox_select");
+		for(var i=0;i<checkbox_select.length;i++){
+			if(checkbox_select[i].checked== true){
+				checkbox_select[i].checked=false;
+			}else{
+				checkbox_select[i].checked=true;
+			}
+		}
+	});
