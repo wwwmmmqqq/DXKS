3a7dd7c16fbd336dc1b11e30ab2f55754cfd443c
lrx <601740275@qq.com>
2018-06-23 10:55:35 +0800

    1

--- examsys/src/cn/examsys/servlet/TheDoor.java
+++ examsys/src/cn/examsys/servlet/TheDoor.java
@@ -7,6 +7,10 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.struts2.ServletActionContext;
 import org.apache.struts2.dispatcher.ng.filter.StrutsPrepareAndExecuteFilter;
 
 public class TheDoor extends StrutsPrepareAndExecuteFilter {
@@ -18,6 +22,14 @@ public class TheDoor extends StrutsPrepareAndExecuteFilter {
 		HttpServletRequest request = (HttpServletRequest) req;
 		String url = request.getRequestURL().toString();
 		System.out.println("visit:" + url);
+		
+		HttpSession session = request.getSession();
+		if (session.getAttribute("user") == null && !url.contains("pages/gy/login.jsp")) {
+			HttpServletResponse response = (HttpServletResponse) res;
+			response.sendRedirect("pages/gy/login.jsp");
+			response.getWriter().write("<script>alert('请先登录');</script>");
+			response.getWriter().flush();
+		}
 		
 		super.doFilter(req, res, chain);
 	}
