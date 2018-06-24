985b5afde077ddeca905ad0ba20391aa65caa99f
huajianyi <1770313147@qq.com>
2018-06-24 09:27:41 +0800

    注册 完善

--- examsys/WebContent/pages/gy/js/register.js
+++ examsys/WebContent/pages/gy/js/register.js
@@ -3,22 +3,36 @@ function register_ajax(){
 	var user_name=$("input[name='user_name']").val();
 	var user_psw=$("input[name='user_psw']").val();
 	var user_email=$("input[name='user_email']").val();
-	
+	var user_psw1=$("input[name='user_psw1']").val();
 	var formData=new FormData();
 		formData.append("user.userId",user_name);
 		formData.append("user.psw",user_psw);
 		formData.append("user.email",user_email);
 		
-		$.ajax({
-			url:"registUser",
-			type:"post",
-			data:formData,
-			//报错加入以下三行，则ajax提交无问题
-			cache:false,
-			processData:false,
-			contentType:false,
-			success: function (data){
-				alert(data.result);//success fail exist
+		if(user_name==""||user_psw==""||user_email==""||user_psw1==""){
+			alert("请输入信息")
+		}else{
+			
+			if(user_psw==user_psw1){
+				
+				$.ajax({
+					url:"registUser",
+					type:"post",
+					data:formData,
+					//报错加入以下三行，则ajax提交无问题
+					cache:false,
+					processData:false,
+					contentType:false,
+					success: function (data){
+						alert(data.result);//success fail exist
+					}
+				});
+			}else{
+				alert("两次密码输入不一致！")
+				
 			}
-		});
+		
+			
+		}
+		
 }
